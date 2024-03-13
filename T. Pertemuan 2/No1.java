f869e40acfb14476ae4fe347014bec8a97b722e0
FadhilahAA <112860183+Fadhilah25@users.noreply.github.com>
2024-03-13 20:43:07 +0700

    Add files via upload

---
+++ T. Pertemuan 2/No1.java
@@ -0,0 +1,13 @@
+package minggu1;
+
+public class No1 {
+  
+    public static void main(String[] args){
+        System.out.println("\n=========================");
+        System.out.println("PROGRAM MENULIS KALIMAT");
+        System.out.println("=========================");
+        System.out.println("\nHalo, Nama saya adalah Fadhilah Aditya Akbar");
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program java\n");
+    }
+}
